a33430f9fce0765e33e8673df6330a9fd3d53d41
johndwatt <jdw23@protonmail.com>
2022-02-08 18:58:37 -0800

    adds dependencies and needed imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
   return (
-    <div className="container">
-      Hello World
-    </div>
+    <Router>
+      <div className="container">
+        Hello World
+      </div>
+    </Router>
+
   );
 }
 
