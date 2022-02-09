b9300116d17e12ad9fc5164b5fd5ffedb8a81bd5
johndwatt <jdw23@protonmail.com>
2022-02-08 17:14:08 -0800

    adds mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
+
+// mongoose connection:
+const uri = 'mongodb://localhost:27017/mern-tutorial-1'
+mongoose.connect(uri);
+mongoose.connection.on('connected', () => {
+    console.log('\x1B[42m%s\x1b[0m', `=== MongoDB connected ===`);
+});
+mongoose.connection.on('error', (error) => {
+    console.log('\x1b[41m%s\x1b[0m', '=== MongoDB connection error ===', error)
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('\x1B[44m%s\x1b[0m', '=== MongoDB disconnected ===');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is live on port: ${PORT}`);
